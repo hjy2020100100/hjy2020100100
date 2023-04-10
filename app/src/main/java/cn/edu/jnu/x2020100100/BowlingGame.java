b91297159d7151efd29bb05c513674068bd291dd
hjy <1807586149@qq.com>
2023-04-10 20:39:44 +0800

    3commit

--- app/src/main/java/cn/edu/jnu/x2020100100/BowlingGame.java
+++ app/src/main/java/cn/edu/jnu/x2020100100/BowlingGame.java
@@ -1,10 +1,12 @@
 package cn.edu.jnu.x2020100100;
 
 public class BowlingGame {
+    int score=0;
     public void roll(int pin) {
+        score+=pin;
     }
 
     public int score() {
-        return 40;
+        return score;
     }
 }
