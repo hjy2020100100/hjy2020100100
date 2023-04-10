628c65633455b26e57c389e3588b95c894a1bb44
hjy <1807586149@qq.com>
2023-04-10 21:59:32 +0800

    最后添加

--- app/src/main/java/cn/edu/jnu/x2020100100/MainActivity.java
+++ app/src/main/java/cn/edu/jnu/x2020100100/MainActivity.java
@@ -3,6 +3,7 @@ package cn.edu.jnu.x2020100100;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonCalculate= this.findViewById(R.id.buton_calculate);
+        EditText editTextScores= this.findViewById(R.id.edittext_frome_scores);
+        TextView textViewScore= this.findViewById(R.id.textview_score);
+        buttonCalculate.setOnClickListener(new View.OnClickListener()){
+            @Override
+            public void onClick(View view){
+                String scores= editTextScores.getText().toString();
+                string [] arrayScore= scores.split(",");
+                BowlingGame game=new BowlingGame();
+                for (int i=0;i< arrayScore.length;i++)
+                {
+                    game.roll(Integer.parseInt()arrayScore[i]);
+
+                }
+                textViewScore.setText("Total score is:"+game.score())
+            }
+        }
     }
 }
